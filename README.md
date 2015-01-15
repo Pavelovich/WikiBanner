7b7efcd32460e986cd2c8b62993ea76948d9199c
Sasha Pavelovich <spavelovich@lexipedium.org>
2015-01-14 22:45:17 -0500

    Adding files

---
+++ README.md
@@ -0,0 +1,6 @@
+WikiBanner
+==========
+
+Adds an easy system for adding and configuring wiki banners on MediaWiki powered websites from the LocalSettings.php file.
+
+Installation instructions can be found at http://www.mediawiki.org/wiki/Extension:WikiBanner
---
+++ WikiBanner.php
@@ -0,0 +1,29 @@
+<?php
+
+$wgExtensionCredits[$type][] = array(
+        'path' => __FILE__,
+        'name' => "WikiBanner",
+        'description' => "Allows top and bottom banners to be added to the wiki as configured in the LocalSettings.php file.",
+//      'descriptionmsg' => "",
+        'version' => 1.1,
+        'author' => "maelstr0m",
+        'url' => "http://www.mediawiki.org/wiki/Extension:WikiBanner",
+);
+
+
+//Explicitly defining global variables
+
+$wgTopBannerCode = '<!-- No Top Banner -->';
+$wgBottomBannerCode = '<!-- No Bottom Banner -->';
+
+//Code for adding the top and bottom banners to the wiki
+
+$wgHooks['BeforePageDisplay'][] = 'WikiBanner';
+function WikiBanner( OutputPage &$out, Skin &$skin ) {
+	global $wgTopBannerCode, $wgBottomBannerCode;
+
+	$out->prependHTML( $wgTopBannerCode );
+	$out->addHTML( $wgBottomBannerCode );
+
+	return TRUE;
+}
